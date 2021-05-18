83be530632a94f39322d40285b8fa6de456dda4f
Vladimír Šnejd <snejdvl1@fel.cvut.cz>
2021-05-18 14:46:26 +0000

    Upload lcd_text.h header file with funcions related to writing out text using font descriptor

---
+++ lcd_text.h
@@ -0,0 +1,10 @@
+#ifndef LCD_TEXT_H
+#define LCD_TEXT_H
+
+#include "font_types.h"
+
+void draw_pixel(int x, int y, unsigned short color, unsigned short *fb);
+void draw_char(int x, int y, font_descriptor_t *fdes, char ch);
+int char_width(font_descriptor_t *fdes, int ch);
+
+#endif
