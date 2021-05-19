b5e493bab8f8e64ac528c9445eef4e183b3193a0
Vladimír Šnejd <snejdvl1@fel.cvut.cz>
2021-05-19 18:16:48 +0000

    Update game.h added knob funtion

--- game.h
+++ game.h
@@ -2,5 +2,6 @@
 #define GAME_H
 
 void keyboard_game(unsigned char *mem_base, unsigned char *parlcd_mem_base, unsigned short *fb);
+void knob_game(unsigned char *mem_base, unsigned char *parlcd_mem_base, unsigned short *fb);
 
 #endif
