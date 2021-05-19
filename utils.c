58bd18bca92ea5256b7ed1c4088c641b8083e95e
Vladimír Šnejd <snejdvl1@fel.cvut.cz>
2021-05-19 18:18:41 +0000

    Update utils.c added funtion for detecting keypress

--- utils.c
+++ utils.c
@@ -3,10 +3,15 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include "utils.h"
 
-int rand_sign() 
+int rand_sign(void) 
 {
   return (float)rand() / ((float)INT32_MAX / 2.0f) <= 0.5 ? -1 : 1;
 }
@@ -22,4 +27,31 @@ int getch(void)
     ch = getchar();
     tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
     return ch;
+}
+
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+ 
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+  ch = getchar();
+ 
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return 1;
+  }
+ 
+  return 0;
 }
