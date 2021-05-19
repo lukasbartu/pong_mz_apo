2af45c95e6278fb0e16fe09071046a35480b748e
Vladimír Šnejd <snejdvl1@fel.cvut.cz>
2021-05-19 17:19:43 +0000

    Update utils.c new fuction - for reading input

--- utils.c
+++ utils.c
@@ -1,3 +1,6 @@
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -6,4 +9,17 @@
 int rand_sign() 
 {
   return (float)rand() / ((float)INT32_MAX / 2.0f) <= 0.5 ? -1 : 1;
+}
+
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
 }
