1c56229227cc153bc3c8adf5f2f2aae627b3aa60
Vladimír Šnejd <snejdvl1@fel.cvut.cz>
2021-05-20 10:03:44 +0000

    Update main.c added contol of menu with knobs, fixed menu functions

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #include "mzapo_parlcd.h"
 #include "mzapo_phys.h"
@@ -14,36 +15,35 @@
 #include "font_types.h"
 #include "utils.h"
 
-
 #define DISPLAY_WIDTH 480
 #define DISPLAY_HEIGHT 320
 
 int main(int argc, char *argv[])
 {
+    unsigned short *fb;
     unsigned char *mem_base;
     unsigned char *parlcd_mem_base;
     uint32_t val_line = 5;
     printf("Alloc FB\n");
-    game.fb = (unsigned short *)malloc(DISPLAY_HEIGHT * DISPLAY_WIDTH * 2); //allocates frame buffer
-    if(!game.fb){
+    fb = (unsigned short *)malloc(DISPLAY_HEIGHT * DISPLAY_WIDTH * 2); //allocates frame buffer
+    if (!fb){
         exit(1);
     }
     printf("Hello world\n");
 
     sleep(1);
 
-  /*
+    /*
    * Setup memory mapping which provides access to the peripheral
    * registers region of RGB LEDs, knobs and line of yellow LEDs.
    */
     mem_base = map_phys_address(SPILED_REG_BASE_PHYS, SPILED_REG_SIZE, 0);
-  /* If mapping fails exit with error code */
+    /* If mapping fails exit with error code */
     if (mem_base == NULL)
         exit(1);
 
     struct timespec loop_delay = {.tv_sec = 0, .tv_nsec = 20 * 1000 * 1000};
-    for (int i = 0; i < 30; i++)
-    {
+    for (int i = 0; i < 30; i++){
         *(volatile uint32_t *)(mem_base + SPILED_REG_LED_LINE_o) = val_line;
         val_line <<= 1;
         printf("LED val 0x%x\n", val_line);
@@ -51,102 +51,119 @@ int main(int argc, char *argv[])
     }
 
     parlcd_mem_base = map_phys_address(PARLCD_REG_BASE_PHYS, PARLCD_REG_SIZE, 0);
-  /* If mapping fails exit with error code */
-    if (parlcd_mem_base == NULL)
+    /* If mapping fails exit with error code */
+    if (parlcd_mem_base == NULL){
         exit(1);
+    }
 
     printf("Font descriptors\n");
-    font_descriptor_t* fdes88 = &font_wArial_88; //  font template
-    font_descriptor_t* fdes44 = &font_wArial_44; //  font template
-    
-    frame_buffer_clear(game.fb);
+    font_descriptor_t *fdes88 = &font_wArial_88; //  font template
+    font_descriptor_t *fdes44 = &font_wArial_44; //  font template
+
+    frame_buffer_clear(fb);
 
     // start of start-up screen ----------------------------------------------------
-    draw_string(105,60,"PONG",4,WHITE_COLOR,fdes88,game.fb);
-    draw_string(80,250,"PRESS ENTER!",11,WHITE_COLOR,fdes44,game.fb); 
-    lcd_draw(parlcd_mem_base, game.fb);
+    draw_string(105, 60, "PONG", 4, WHITE_COLOR, fdes88, fb);
+    draw_string(80, 250, "PRESS ENTER!", 11, WHITE_COLOR, fdes44, fb);
+    lcd_draw(parlcd_mem_base, fb);
 
     printf("PRESS ENTER TO CONTINUE!\n");
     getchar(); // wait for enter
     // end of start-up screen --------------------------------------------------------
 
-    frame_buffer_clear(game.fb);
+    frame_buffer_clear(fb);
 
     // start of menu screen ----------------------------------------------------------
-    draw_string(105,15,"MENU",4,YELLOW_COLOR,fdes88,game.fb);
-    draw_string(30,100,"Standard mode",13,BLUE_COLOR,fdes44,game.fb); // option 0
-    draw_string(10,140,"Speed mode",10,YELLOW_COLOR,fdes44,game.fb);  // option 1
-    draw_string(10,180,"Arcade mode",11,YELLOW_COLOR,fdes44,game.fb); // option 2
-    lcd_draw(parlcd_mem_base, game.fb);
+    draw_string(105, 15, "MENU", 4, YELLOW_COLOR, fdes88, fb);
+    draw_string(30, 100, "Keyboard mode", 13, BLUE_COLOR, fdes44, fb); // option 0
+    draw_string(10, 140, "Knob mode", 10, YELLOW_COLOR, fdes44, fb);   // option 1
+    draw_string(10, 180, "exit", 11, YELLOW_COLOR, fdes44, fb);        // option 2
+    lcd_draw(parlcd_mem_base, fb);
 
-
-    char c;
+    char c = '0';
     int option = 0;
-    while((c = getch()) != 'q'){
-        switch (c)
-        {
-        case '-': // menu up
-            if(option-1 < 0 ){
+    bool quit = false;
+    bool newentry = false;
+    uint32_t rgb_knobs_value;
+    uint8_t pushedknob;
+    while (!quit){
+        if (kbhit()){
+            c = getch();
+        }
+        rgb_knobs_value = *(volatile uint32_t *)(mem_base + SPILED_REG_KNOBS_8BIT_o);
+        pushedknob = (uint8_t)(rgb_knobs_value >> 24);
+
+        if (pushedknob == 0x04 || c == '-'){ //red knob || -
+            if (option - 1 < 0){
                 option = 2;
             }else{
-                option = option -1; 
+                option = option - 1;
             }
-            break;
-        case '+': // menu down
-            if(option+1 > 2 ){
+            c = '0';
+            newentry = true;
+        }else if (pushedknob == 0x01 || c == '+'){ //blue knob || +
+            if (option + 1 > 2){
                 option = 0;
             }else{
-                option = option +1; 
+                option = option + 1;
             }
-            break;
-        case 0x0A: // enter
-            if(option == 0){
-                keyboard_game(mem_base, parlcd_mem_base, game.fb);
+            c = '0';
+            newentry = true;
+        }else if (pushedknob == 0x02 || c == 0x0A){ //green knob || enter
+            c = '0';
+            newentry = true;
+            if (option == 0){
+                keyboard_game(mem_base, parlcd_mem_base, fb);
+            }else if (option == 1){
+                knob_game(mem_base, parlcd_mem_base, fb);
+            }else if (option == 2){
+                quit = true;
             }
-            break;
-        default:
-            break;
         }
-        switch (option)
-        {
-        case 0: // option 0 selected
-            frame_buffer_clear(game.fb);
-            draw_string(105,15,"MENU",4,YELLOW_COLOR,fdes88,game.fb);
-            draw_string(30,100,"Standard mode",13,BLUE_COLOR,fdes44,game.fb); // option 0
-            draw_string(10,140,"Speed mode",10,YELLOW_COLOR,fdes44,game.fb);  // option 1
-            draw_string(10,180,"Arcade mode",11,YELLOW_COLOR,fdes44,game.fb); // option 2
-            lcd_draw(parlcd_mem_base, game.fb);
-            break;
-        case 1: // option 1 selected
-            frame_buffer_clear(game.fb);
-            draw_string(105,15,"MENU",4,YELLOW_COLOR,fdes88,game.fb);
-            draw_string(10,100,"Standard mode",13,YELLOW_COLOR,fdes44,game.fb); // option 0
-            draw_string(30,140,"Speed mode",10,BLUE_COLOR,fdes44,game.fb);  // option 1   
-            draw_string(10,180,"Arcade mode",11,YELLOW_COLOR,fdes44,game.fb); // option 2 
-            lcd_draw(parlcd_mem_base, game.fb); 
-            break;
-        case 2: // option 2 selected
-            frame_buffer_clear(game.fb);
-            draw_string(105,15,"MENU",4,YELLOW_COLOR,fdes88,game.fb);
-            draw_string(10,100,"Standard mode",13,YELLOW_COLOR,fdes44,game.fb); // option 0
-            draw_string(10,140,"Speed mode",10,YELLOW_COLOR,fdes44,game.fb);  // option 1
-            draw_string(30,180,"Arcade mode",11,BLUE_COLOR,fdes44,game.fb); // option 2
-            lcd_draw(parlcd_mem_base, game.fb);
-            break;
-        default:
-            break;
-        }  
+
+        if (newentry){
+            newentry = false;
+            switch (option)
+            {
+            case 0: // option 0 selected
+                frame_buffer_clear(fb);
+                draw_string(105, 15, "MENU", 4, YELLOW_COLOR, fdes88, fb);
+                draw_string(30, 100, "Keyboard mode", 13, BLUE_COLOR, fdes44, fb); // option 0
+                draw_string(10, 140, "Knob mode", 10, YELLOW_COLOR, fdes44, fb);   // option 1
+                draw_string(10, 180, "exit", 11, YELLOW_COLOR, fdes44, fb);        // option 2
+                lcd_draw(parlcd_mem_base, fb);
+                break;
+            case 1: // option 1 selected
+                frame_buffer_clear(fb);
+                draw_string(105, 15, "MENU", 4, YELLOW_COLOR, fdes88, fb);
+                draw_string(10, 100, "Keyboard mode", 13, YELLOW_COLOR, fdes44, fb); // option 0
+                draw_string(30, 140, "Knob mode", 10, BLUE_COLOR, fdes44, fb);       // option 1
+                draw_string(10, 180, "exit", 11, YELLOW_COLOR, fdes44, fb);          // option 2
+                lcd_draw(parlcd_mem_base, fb);
+                break;
+            case 2: // option 2 selected
+                frame_buffer_clear(fb);
+                draw_string(105, 15, "MENU", 4, YELLOW_COLOR, fdes88, fb);
+                draw_string(10, 100, "Keyboard mode", 13, YELLOW_COLOR, fdes44, fb); // option 0
+                draw_string(10, 140, "Knob mode", 10, YELLOW_COLOR, fdes44, fb);     // option 1
+                draw_string(30, 180, "exit", 11, BLUE_COLOR, fdes44, fb);            // option 2
+                lcd_draw(parlcd_mem_base, fb);
+                break;
+            default:
+                break;
+            }
+        }
     }
 
-    frame_buffer_clear(game.fb);
-    draw_string(130,160,"END",3,WHITE_COLOR,fdes88,game.fb);
-    lcd_draw(parlcd_mem_base, game.fb);
-// end of menu screen ------------------------------------------------------------
+    frame_buffer_clear(fb);
+    draw_string(130, 160, "END", 3, WHITE_COLOR, fdes88, fb);
+    lcd_draw(parlcd_mem_base, fb);
+    // end of menu screen ------------------------------------------------------------
 
-    if(game.fb){
-        free(game.fb);
+    if (fb){
+        free(fb);
     }
     printf("Goodbye world\n");
 
-  return 0;
+    return 0;
 }
