f67e1b7874ebef09448831ec6df74d9525a1a2dd
Lukas Bartunek <bartulu2@fel.cvut.cz>
2021-05-20 22:32:23 +0200

    Added new colors

--- colors.h
+++ colors.h
@@ -5,5 +5,9 @@
 #define WHITE_COLOR 0xFFFF
 #define YELLOW_COLOR 0xFF00
 #define BLUE_COLOR  0x8efe
+#define PURPLE_COLOR  0x801F
+#define PINK_COLOR  0xFC1F
+#define GREEN_COLOR  0x0FF0
+
 
 #endif
